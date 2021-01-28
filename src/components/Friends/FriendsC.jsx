cacd9de606ad1dd45ef464be941654a33c1d1712
Anton <anton.valt@gmail.co>
2021-01-29 00:24:40 +0200

    53 lesson (refactor function to component)

--- src/components/Friends/FriendsC.jsx
+++ src/components/Friends/FriendsC.jsx
@@ -2,37 +2,43 @@ import React from 'react';
 import style from './Friends.module.css';
 import * as axios from "axios";
 import userLogo from "../../img/user_logo.png"
+import icon_m_01 from "../../Redux/img/icon_m_01.png";
+import icon_m_02 from "../../Redux/img/icon_m_02.png";
+import icon_m_03 from "../../Redux/img/icon_m_03.png";
+import icon_m_04 from "../../Redux/img/icon_m_04.png";
+import icon_f_01 from "../../Redux/img/icon_f_01.png";
+import icon_f_02 from "../../Redux/img/icon_f_02.png";
+import icon_f_03 from "../../Redux/img/icon_f_03.png";
+import icon_f_04 from "../../Redux/img/icon_f_04.png";
 
 
-class Friends extends React.Component {
+const Friends = (props) => {
 
-    constructor(props){
-        super(props);
+    if (props.userList.userList.length === 0) {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 props.setUsers(response.data.items)
             });
     }
 
-    render() {
-        return (
-            <div>
-                {
-                    this.props.userList.userList.map(user => <div key={user.id}>
+    return (
+        <div>
+            {
+                props.userList.userList.map(user => <div key={user.id}>
                     <span>
                         <div><img className={style.avatarImg}
                                   src={user.photos.small != null ? user.photos.small : userLogo} alt=""/></div>
                         <div>
                             {user.followed
                                 ? <button onClick={() => {
-                                    this.props.unfollow(user.id)
+                                    props.unfollow(user.id)
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    this.props.follow(user.id)
+                                    props.follow(user.id)
                                 }}>Follow</button>}
                         </div>
                     </span>
-                        <span>
+                    <span>
                         <span>
                             <div>{user.name}</div>
                             <div>{user.status}</div>
@@ -42,12 +48,10 @@ class Friends extends React.Component {
                             <div>{"user.location.country"}</div>
                         </span>
                     </span>
-                    </div>)
-                }
-            </div>
-        );
-    }
+                </div>)
+            }
+        </div>
+    );
 }
-
 
 export default Friends;
