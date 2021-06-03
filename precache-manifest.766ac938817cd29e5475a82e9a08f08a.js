0b7f0841d5cd43f24f8c0d5dbbd6ac0aa9d0f470
Anton <anton.valt@gmail.co>
2021-06-03 21:51:29 +0300

    Updates

---
+++ precache-manifest.766ac938817cd29e5475a82e9a08f08a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64580c5eeb95d8387f05858ccaed474",
+    "url": "/My-Fifst-Project-SocialNetwork/index.html"
+  },
+  {
+    "revision": "eb79e8435f227925fc54",
+    "url": "/My-Fifst-Project-SocialNetwork/static/css/3.c937bc3a.chunk.css"
+  },
+  {
+    "revision": "34452f59239766cfd17b",
+    "url": "/My-Fifst-Project-SocialNetwork/static/css/main.22fa5d68.chunk.css"
+  },
+  {
+    "revision": "5c5cd8aa09b09b2b1ba7",
+    "url": "/My-Fifst-Project-SocialNetwork/static/js/2.4b66a87e.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/My-Fifst-Project-SocialNetwork/static/js/2.4b66a87e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb79e8435f227925fc54",
+    "url": "/My-Fifst-Project-SocialNetwork/static/js/3.49fc7201.chunk.js"
+  },
+  {
+    "revision": "34452f59239766cfd17b",
+    "url": "/My-Fifst-Project-SocialNetwork/static/js/main.f52bb504.chunk.js"
+  },
+  {
+    "revision": "5e496fcc6fd5c56c5ffd",
+    "url": "/My-Fifst-Project-SocialNetwork/static/js/runtime-main.b7034d92.js"
+  },
+  {
+    "revision": "892092cf90ecdaa17094f783f2abe5e2",
+    "url": "/My-Fifst-Project-SocialNetwork/static/media/preload.892092cf.svg"
+  },
+  {
+    "revision": "6efb8305e609228e84e9910fea042b44",
+    "url": "/My-Fifst-Project-SocialNetwork/static/media/user-logo.6efb8305.png"
+  }
+]);
