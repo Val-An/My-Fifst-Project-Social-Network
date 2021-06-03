a812b2a2efaa0069111f64799f18195563dcd98e
Anton <anton.valt@gmail.co>
2021-06-03 21:09:53 +0300

    94 lesson (React.lazy / React.Suspense)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {Route, withRouter} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import FriendsContainer from "./components/Friends/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
@@ -15,7 +15,9 @@ import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./Redux/appReducer";
 import Preloader from "./components/Common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense";
 
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"))
 
 class App extends React.Component {
     componentDidMount() {
@@ -36,7 +38,7 @@ class App extends React.Component {
                     <div className="content">
                         <Route path='/profile/:userId?' render={() => <ProfileContainer />}/>
                         <Route path='/friends' render={() => <FriendsContainer/>}/>
-                        <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                        <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
                         <Route path='/news' render={() => <News/>}/>
                         <Route path='/music' render={() => <Music/>}/>
                         <Route path='/settings' render={() => <Settings/>}/>
